2a8c2e568dc7612c8e82e0c9ea88ecf9ac5d5d38
Mary Rose <SuicideFox@icloud.com>
2025-06-18 04:22:50 +0200

    Niiagara UI Renderer

--- Source/MyCodes/Draugr.cpp
+++
@@ -1,2 +0,0 @@
-#include "C:\Users\SuiciFox\Saved Games\Unreal\Eir\Intermediate\Build\Win64\x64\EirEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp17.InclOrderOldest.h"
-#include "Draugr.h"
